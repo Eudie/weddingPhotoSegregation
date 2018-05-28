f3231e4b96c13f021ebb357e8b700b0e5a763a1f
Eudie <utkarsh.dubey.26@gmail.com>
2018-05-28 16:07:41 +0530

    completed extract_face

--- photoSegregator.py
+++ photoSegregator.py
@@ -12,24 +12,49 @@ import pandas as pd
 import numpy as np
 import os
 import cv2
+import face_recognition
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
 class Segregator:
-    def __init__(self, dir):
-        self.dir = dir
+    """
+    This module will be use to segregating the images.
+    """
+    def __init__(self, directory):
+        self.directory = directory
+        self.file_names = [os.path.join(root, filename)
+                           for root, directories, filenames in os.walk(self.directory)
+                           for filename in filenames
+                           if filename.lower().endswith(('.png', '.jpg', '.jpeg'))]
 
-    def extract_faces(self, face_dir):
+    def extract_faces(self, scale_down=10):
         """
         This function extract all the faces from the photos in the main directory
-        :param face_dir: directory where we want to save the faces, which we will used in unsurervised then tagging.
-        :return: all the faces in all photos
+        :scale_down: factor by which images will be scaled down for processing
         """
         # TODO
+        self.face_dir = os.path.join(self.directory, '..', 'face_' + os.path.basename(os.path.normpath(self.directory)))
+        if not os.path.exists(self.face_dir):
+            os.makedirs(self.face_dir)
 
-        return 0
+        for file in self.file_names:
+            image = cv2.imread(file)
+            width = int(image.shape[1] / scale_down)
+            height = int(image.shape[0] / scale_down)
+            dim = (width, height)
+            resize_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+            # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            face_locations = face_recognition.face_locations(resize_image, model="cnn")
+            for index, face in enumerate(face_locations):
+                face = tuple(scale_down * x for x in face)
+                crop_face = image[face[0]:face[2], face[3]:face[1], :]
+                face_file_name = os.path.join(self.face_dir, "__".join((str(index), os.path.split(file)[1])))
+                cv2.imwrite(face_file_name, crop_face)
 
 
-    def cluster_faces(self, face_dir, clust_dir, no_of_clusters = 'Auto'):
+    def cluster_faces(self, clust_dir, no_of_clusters = 'Auto'):
         """
         It uses unsupervised method to cluster images. The purpose of this function is to help im manual tagging.
         By using this we will tagg the image which we will then used to classify
@@ -40,6 +65,10 @@ class Segregator:
         """
 
         # TODO
+        self.cluster_dir = os.path.join(self.directory, 'cluster_faces')
+
+        if not os.path.exists(self.cluster_dir):
+            os.makedirs(self.cluster_dir)
 
         return 0
 
